c5ad79a64e941a3b21da468c4b68ac49520b5a40
Francisco Quintero Coronell <frajaquico@yahoo.es>
2021-04-23 16:00:26 -0500

    Remove code that is not enabled by gems

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,8 +5,8 @@
 
 require("@rails/ujs").start()
 require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+// require("@rails/activestorage").start()
+// require("channels")
 require("local-time").start()
 
 window.Rails = Rails
