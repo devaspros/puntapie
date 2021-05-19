e9c3b2d2940bd3dac7464f58a3d20113a300551a
Francisco Quintero Coronell <frajaquico@yahoo.es>
2021-05-18 20:05:33 -0500

    Remove turbolinks javascript code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,17 +4,9 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
-// require("@rails/activestorage").start()
-// require("channels")
 require("local-time").start()
 
 window.Rails = Rails
 
 import 'bootstrap'
 import 'data-confirm-modal'
-
-$(document).on("turbolinks:load", () => {
-  $('[data-toggle="tooltip"]').tooltip()
-  $('[data-toggle="popover"]').popover()
-})
