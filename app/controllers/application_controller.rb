87aaa3246173b499833560b4f3cc90df12286c11
Francisco Quintero Coronell <frajaquico@yahoo.es>
2024-10-11 16:41:08 -0500

    Configure sidebar theme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,17 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
-  protected
+  layout :layout_by_resource
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "login"
+    else
+      "application"
+    end
   end
 end
